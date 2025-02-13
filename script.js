bc1d3dacbb00e8cad2cf28747e49c810208d8859
Sylyx-7 <giuliaherrmann2501@gmail.com>
2025-02-13 12:32:20 +0100

    12

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function toggleMenu() {
 let menu = document.getElementById("navLinks");
 menu.classList.toggle("show");
 }
+
+document.getElementById("darkModeToggle").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+}); 
