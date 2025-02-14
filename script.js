05c6fba46ca9cd27ce320822d6b85979d58a93db
Sylyx-7 <giuliaherrmann2501@gmail.com>
2025-02-14 10:16:12 +0100

    17

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ menu.classList.toggle("show");
 
 document.getElementById("darkModeToggle").addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
-}); 
+});
+
+
+document.querySelectorAll('.toggle-btn').forEach(button => {
+button.addEventListener('click', function() {
+const description = this.previousElementSibling;
+const isVisible = description.style.display === 'block';
+
+if (isVisible) {
+description.style.display = 'none';
+this.textContent = 'Mehr anzeigen';
+} else {
+description.style.display = 'block';
+this.textContent = 'Weniger anzeigen';
+      }
+    });
+  });
