334eec5d330c2b538ab421d67f3a8c62e8a2380f
Sylyx-7 <giuliaherrmann2501@gmail.com>
2025-02-18 11:39:32 +0100

    script datein aus html ind script.js gemacht

--- script.js
+++ script.js
@@ -55,3 +55,109 @@ function updateProgressBar() {
     const progressBar = document.getElementById('progress-bar');
     progressBar.style.height = scrollPercentage + '%';
 }
+
+
+console.log("Willkommen auf meinem Portfolio");
+        
+setTimeout(function() {
+    console.log("Schön dich hier zu sehen");
+}, 2000);
+
+
+  
+const navItems = ["Startseite", "Über mich", "Kontakt"];
+const nav = document.getElementById("navigation");
+navItems.forEach(item => {
+    const link = document.createElement("a");
+    link.href = "#"; 
+    link.textContent = item;
+    nav.appendChild(link);
+});
+
+
+const email = "lisaweber@webentwicklerin.de";
+const link = document.createElement("a");
+link.href = "mailto:" + email;
+link.textContent = email;
+document.getElementById("emailLink").appendChild(link);
+
+
+document.getElementById('kategorieFilter').addEventListener('change', function() {
+  const filterValue = this.value;
+  const projects = document.querySelectorAll('.projekt');
+       
+   projects.forEach(function(project) {
+   const projectCategory = project.getAttribute('data-category');
+           
+   if (filterValue === 'all' || filterValue === projectCategory) {
+   project.style.display = 'block'; 
+   } else {
+   project.style.display = 'none';
+   }
+   });
+   });
+
+
+   function smoothScroll() {
+    document.getElementById("ziel").scrollIntoView({ 
+    behavior: "smooth", 
+    block: "start" 
+    });
+    }
+
+
+
+    fetch('projekt.json')
+    .then(response => response.json())
+    .then(data => {
+        const container = document.getElementById('projekt-container');
+        container.innerHTML = ''; 
+
+        data.forEach(projekt => {
+            const div = document.createElement('div');
+            div.classList.add('projekt');
+            div.innerHTML = `
+            <h2>${projekt.name}</h2>
+            <a href="${projekt.link}" "target= _blank"></a>
+            `;
+            container.appendChild(div);
+        });
+    })
+    .catch(error => {
+        console.error('Fehler beim Laden der JSON:', error);
+        document.getElementById('projekt-container').innerText = "Das Projekt konnte nicht geladen werden.";
+    });
+
+
+
+    const testimonials = [
+      { text: '"Super Portfolio!"', author: "- Giulia H."}
+    ];
+    let index = 0;
+    function nextTestimonial () {
+      index = (index + 1) % testimonials.length;
+      document.getElementById("feedback").textContent = testimonials[index].text
+      document.getElementById("author").textContent = testimonials[author].text
+    }
+
+
+    document.getElementById("form").onsubmit = function(event){
+      event.preventDeafault();
+  
+      let data = {
+        name : document.getElementById("name").value,
+        password : document.getElementById("password").value
+      };
+      console.log(JSON.stringfy(data));
+    };
+
+
+
+    window.onscroll = function() { updateProgress()};
+
+    function updateProgress(){
+      let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+      let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      let scrollPercent = (scrollTop / scrollHeight) * 10;
+      document.getElementById("progressBar").style.height = scrollPercent + "1%";
+    }
